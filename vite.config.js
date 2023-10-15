b0ac3c85f4f733f17ba981d1f870d0620beb2bf3
黄艳鹏 <hyp0105@qq.com>
2023-10-15 17:10:37 +0800

    自动部署demo尝试

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
